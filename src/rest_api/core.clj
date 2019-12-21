29956ff4893f4cc20a1c6e27292f766a1854c312
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-21 18:18:27 -0500

    aight lets do it

--- src/rest_api/core.clj
+++ src/rest_api/core.clj
@@ -1,4 +1,11 @@
 (ns rest-api.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as ppr]
+            [clojure.string :as str]
+            [clojure.data.json :as json])
   (:gen-class))
 
 (defn -main

1dd9657caa75812df2c9a71ea8e18a2dffcbc953
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-21 19:28:31 -0500

    tweaking bugs

--- src/rest_api/simple_body_page.clj
+++ src/rest_api/simple_body_page.clj
@@ -1,5 +1,7 @@
 (ns rest-api.simple-body-page
-    (:require [clojure.pprint :as ppr]))
+    (:require [clojure.pprint :as ppr]
+              [clojure.string :as str]
+              [clojure.data.json :as json]))
 
 (defn simple-body-page [req]
     {:status 200
@@ -18,16 +20,26 @@
      :headers {"Content-Type" "text/html"}
      :body (-> (ppr/pprint req)
                (str "Hello " (:name (:params req))))})
-
+; :name works because we used ?name= as a query string, and our params are turned into a map
+; in the request object
 (def people-collection (atom []))
 
-(defn add-person [f-name l-name]
-    (swap! people-collection conj {:firstname (str/capitalize (first f-name))
-                                   :lasrname (str/capitalize (first l-name))}))
-(add-person "Lore" "Silver")
-(add-person "Klassic" "Kloudhead")
+; (defn add-person [f-name l-name]
+;     (swap! people-collection conj {:firstname (str/capitalize f-name)
+;                                    :lastname (str/capitalize l-name)}))
+; (add-person "Lore" "Silver")
+; (add-person "Klassic" "Kloudhead")
 
 (defn people-handler [req]
     {:status 20
      :headers {"Content-Type" "text/json"}
      :body (str (json/write-str @people-collection))})
+
+(defn get-parameter [req p-name]
+    (get (:param req) p-name))
+
+(defn add-person [req & args]
+ {:status 200
+    :headers {"Content-Type" "text/json"}
+    :body (-> (let [p (partial get-parameter req)]
+                (str (json/write-str (add-person (p :firstname) (p :lastname))))))})

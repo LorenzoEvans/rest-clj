c612fb74875425e5adcea351addbad910a69ccda
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-27 19:20:06 -0500

    aawwww boy, time to get my sql legs back

--- src/rest_api/sforc/db/sql/articles.sql
+++ src/rest_api/sforc/db/sql/articles.sql
@@ -7,5 +7,30 @@ create table articles (
   id    integer auto_increment primary key,
   title   varchar(40),
   body text,
+  tags array,
   created_at timestamp not null default current_timestamp     
 )
+
+-- :name drop-articles-table
+-- :doc Drop articles table iff it exists
+
+drop table if exists articles
+
+-- :name insert-article :! :n
+-- :doc Insert a single article
+
+insert into articles (title, body)
+values (:title, :body)
+
+-- :name insert-articles :! :n
+-- :doc Insert multiple articles
+
+insert into articles (title, body)
+values :tuple* :articles
+
+-- :name insert-tag :! :n
+-- :doc Insert single tag into article
+insert into articles (tag)
+values (:id, tag)
+where id = :id
+
